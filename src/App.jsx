50bd2af445d535b18bac42bb83583836949ac626
TreatEvlar <kuldeepkumarkumar1987@gmail.com>
2025-06-03 19:30:09 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // src/App.jsx
 import { useState } from "react";
 import './App.css';
-import Header from './MyComponents/Header';
-import Hero from "./MyComponents/Hero-section";
-import Mainstore from "./MyComponents/Mainstore";
-import Ministore from "./MyComponents/ministore";
-import Microstore from "./MyComponents/microstore";
-import Ads from "./MyComponents/ads";
-import Weapon from "./MyComponents/Weapon";
-import Armor from "./MyComponents/Armor";
-import Slider from "./MyComponents/Slider";
-import Shield from "./MyComponents/Shield";
-import Blog from "./MyComponents/Blogs";
-import Footer from "./MyComponents/footer";
+import {Header} from './MyComponents/Header';
+import {Hero} from "./MyComponents/Hero-section";
+import {Mainstore} from "./MyComponents/Mainstore";
+import {Ministore} from "./MyComponents/Ministore";
+import {Microstore} from "./MyComponents/microstore";
+import {Ads} from "./MyComponents/ads";
+import {Weapon} from "./MyComponents/Weapon";
+import {Armor} from "./MyComponents/Armor";
+import {Slider} from "./MyComponents/Slider";
+import {Shield} from "./MyComponents/Shield";
+import {Blog} from "./MyComponents/Blogs";
+import {Footer} from "./MyComponents/footer";
 
 function App() {
   return (
