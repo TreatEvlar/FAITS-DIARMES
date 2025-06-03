3c6a7482c22835529e596db5f5046022191841cc
TreatEvlar <kuldeepkumarkumar1987@gmail.com>
2025-06-03 19:47:05 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // src/App.jsx
 import { useState } from "react";
 import './App.css';
-import {Header} from './MyComponents/Header';
-import {Hero} from "./MyComponents/Hero-section";
-import {Mainstore} from "./MyComponents/Mainstore";
-import {Ministore} from "./MyComponents/Ministore";
-import {Microstore} from "./MyComponents/Microstore";
-import {Ads} from "./MyComponents/ads";
-import {Weapon} from "./MyComponents/Weapon";
-import {Armor} from "./MyComponents/Armor";
-import {Slider} from "./MyComponents/Slider";
-import {Shield} from "./MyComponents/Shield";
-import {Blog} from "./MyComponents/Blogs";
-import {Footer} from "./MyComponents/footer";
+import Header from "./MyComponents/Header";
+import HeroSection from "./MyComponents/HeroSection"; 
+import Mainstore from "./MyComponents/Mainstore";
+import Ministore from "./MyComponents/Ministore";
+import Microstore from "./MyComponents/Microstore";
+import Ads from "./MyComponents/Ads";
+import Weapon from "./MyComponents/Weapon";
+import Armor from "./MyComponents/Armor";
+import Slider from "./MyComponents/Slider";
+import Shield from "./MyComponents/Shield";
+import Blog from "./MyComponents/Blogs";
+import Footer from "./MyComponents/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Hero/>
+      <HeroSection/>
       <Mainstore/>
       <Ministore/>
       <Microstore/>
--- src/MyComponents/HeroSection.jsx
+++ src/MyComponents/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Hero() {
+export default function HeroSection() {
   return (
     <div className="hero-section">
       <div className="container">
